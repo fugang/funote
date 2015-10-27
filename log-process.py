67111b07bed7662a992f270d14a3c3c11361fe2e
root <root@kl-glance1.kl-glance1>
2015-10-26 21:56:46 -0500

    update new

--- log-process.py
+++ log-process.py
@@ -34,7 +34,12 @@ def get_files(requests,n):
     for req in requests:
         #req[2] for req file match, change to
         #data you want to count totals
-        requested_files.append(req[n])
+        if n == -1:
+            ipaddr = req[0]
+            acces_url = req[2]
+            requested_files.append(ipaddr+"@"+acces_url)
+        else:
+            requested_files.append(req[n])
     return requested_files
 
 def file_occur(files):
@@ -45,11 +50,15 @@ def file_occur(files):
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         n = 0
-    n = int(sys.argv[1])
+    else:
+        n = int(sys.argv[1])
     for i in os.listdir("/var/log/nginx/"):
         if i.find("access") == -1:
+            continue
+        if i.find("gz") != -1:
             continue
         name = "/var/log/nginx/"+i 
         log_file = open(name, 'r')
         process_log(log_file, n)
-    print d
+    for k,v in d.iteritems():
+        print k, "   ", v 
